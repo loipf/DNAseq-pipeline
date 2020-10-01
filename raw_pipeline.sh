d2cd02a1d21ed2ba1307a342fe49e5223ab90689
loipf <loipfins@in.tum.de>
2020-10-01 11:19:23 +0200

    fixed small stuff

--- raw_pipeline.sh
+++ raw_pipeline.sh
@@ -8,7 +8,6 @@ out_dir="/home/stefan/Documents/umcg/bash_dna-seq_pipeline"
 
 tool_fastqc="/home/stefan/tools/FastQC/fastqc"
 tool_cutadapt="/home/stefan/.local/bin/cutadapt"
-tool_picard="/home/stefan/tools/picard.jar"
 tool_multiqc="/home/stefan/miniconda3/bin/multiqc"
 tool_samtools="/home/stefan/tools/samtools-1.10/samtools"
 tool_bwa="/home/stefan/tools/bwa/bwa"
@@ -29,13 +28,14 @@ mkdir -p $out_dir/data/reads_prepro
 
 
 ### download reference genome
-#curl ftp://ftp.ensembl.org/pub/release-$ensembl_release/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz > $out_dir/data/Homo_sapiens.GRCh38.dna.alt.fa.gz
+curl ftp://ftp.ensembl.org/pub/release-$ensembl_release/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.MT.fa.gz > $out_dir/data/Homo_sapiens.GRCh38.dna.alt.fa.gz
 
 
 ### create BWA index and move in separate folder
-#$tool_bwa index $out_dir/data/Homo_sapiens.GRCh38.dna.alt.fa.gz
-#mkdir -p $out_dir/data/BWA_index
-#mv -t $out_dir/data/BWA_index/ *.amb *.ann *.bwt *.pac *.sa
+$tool_bwa index $out_dir/data/Homo_sapiens.GRCh38.dna.alt.fa.gz
+cd $out_dir/data
+mkdir -p BWA_index
+mv -t BWA_index *.amb *.ann *.bwt *.pac *.sa
 
 
 ### trim reads
@@ -55,7 +55,7 @@ do
     mkdir -p $out_dir/data/reads_raw/$sample_id
     cd $out_dir/data/reads_raw/$sample_id
 
-    #$tool_fastqc -a $adapter_seq_file -t $num_threads --outdir . --noextract $read_files 
+    $tool_fastqc -a $adapter_seq_file -t $num_threads --outdir . --noextract $read_files 
 
 
     ### preprocess reads
@@ -63,10 +63,10 @@ do
     cd $out_dir/data/reads_prepro/$sample_id
     
     ### TODO change minimum length - or remove low quality reads
-    #$tool_cutadapt --cores=$num_threads --max-n 0.1 --minimum-length 10 --discard-trimmed --pair-filter=any -b $adapter_5 -B $adapter_3 -o $sample_id\_prepro_1.fastq.gz -p $sample_id\_prepro_2.fastq.gz $read_files > cutadapt_output.txt
+    $tool_cutadapt --cores=$num_threads --max-n 0.1 --minimum-length 10 --discard-trimmed --pair-filter=any -b $adapter_5 -B $adapter_3 -o $sample_id\_prepro_1.fastq.gz -p $sample_id\_prepro_2.fastq.gz $read_files > cutadapt_output.txt
 
     read_files_prepro=$(find $PWD -name "*.fastq.gz")
-    #$tool_fastqc -a $adapter_seq_file -t $num_threads --outdir . --noextract $read_files_prepro
+    $tool_fastqc -a $adapter_seq_file -t $num_threads --outdir . --noextract $read_files_prepro
 
 
     ### mapping process
@@ -75,15 +75,20 @@ do
 	### TODO check multiple threads add up ? 
     mkdir -p $out_dir/data/reads_mapped/$sample_id
     cd $out_dir/data/reads_mapped/$sample_id
-    #$tool_bwa mem -Y -t $num_threads -K 100000000 $out_dir/data/BWA_index/Homo_sapiens.GRCh38.dna.alt.fa.gz $read_files_prepro \
-	#| $tool_samtools view -@ $num_threads -h -b - \
-    #| $tool_samtools sort -n -@ $num_threads - \
-	#| $tool_samtools fixmate -m -@ $num_threads - - \
-	#| $tool_samtools sort -@ $num_threads - \
-	#| $tool_samtools markdup -@ $num_threads -f $sample_id\_markdup_stats.txt - $sample_id.bam
+    $tool_bwa mem -Y -t $num_threads -K 100000000 $out_dir/data/BWA_index/Homo_sapiens.GRCh38.dna.alt.fa.gz $read_files_prepro \
+	| $tool_samtools view -@ $num_threads -h -b - \
+    | $tool_samtools sort -n -@ $num_threads - \
+	| $tool_samtools fixmate -m -@ $num_threads - - \
+	| $tool_samtools sort -@ $num_threads - \
+	| $tool_samtools markdup -@ $num_threads -f $sample_id\_markdup_stats.txt - $sample_id.bam
 
-	#$tool_samtools index -@ $num_threads $sample_id.bam
-	#$tool_samtools stats -@ $num_threads $sample_id.bam > $sample_id\_stats.txt
+	$tool_samtools index -@ $num_threads $sample_id.bam
+	$tool_samtools stats -@ $num_threads $sample_id.bam > $sample_id\_stats.txt
+
+### TODO discuss, keep only main chromosomes ? 
+# https://www.biostars.org/p/361067/
+#samtools idxstats in.bam | cut -f 1 | grep -vE 'chrM|_random|chrU|chrEBV|\*' | \
+#xargs samtools view -f 1 -F 1284 -q 20 -o out.bam in.bam
 
 
 done
@@ -93,7 +98,7 @@ done
 ############################################
 ### create quality reports
 
-#mkdir -p $out_dir/data/quality_reports
+mkdir -p $out_dir/data/quality_reports
 
 read_files_mapped=$(find $out_dir/data/reads_mapped -name "*.bam")
 
@@ -101,21 +106,21 @@ read_files_mapped=$(find $out_dir/data/reads_mapped -name "*.bam")
 ############################################
 ### deeptools analysis
 mkdir -p $out_dir/data/reads_mapped/_deepTools
-#multiBamSummary bins -p $num_threads --smartLabels --bamfiles $read_files_mapped -o $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz
-#plotCorrelation --corData $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz --corMethod spearman --whatToPlot heatmap --outFileCorMatrix $out_dir/data/reads_mapped/_deepTools/plotCorrelation_matrix.tsv
-#plotPCA --corData $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz --outFileNameData $out_dir/data/reads_mapped/_deepTools/plotPCA_matrix.tsv
+multiBamSummary bins -p $num_threads --smartLabels --bamfiles $read_files_mapped -o $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz
+plotCorrelation --corData $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz --corMethod spearman --whatToPlot heatmap --outFileCorMatrix $out_dir/data/reads_mapped/_deepTools/plotCorrelation_matrix.tsv
+plotPCA --corData $out_dir/data/reads_mapped/_deepTools/multiBamSummary.npz --outFileNameData $out_dir/data/reads_mapped/_deepTools/plotPCA_matrix.tsv
 
-#plotCoverage -p $num_threads --ignoreDuplicates --bamfiles $read_files_mapped --outRawCounts $out_dir/data/reads_mapped/_deepTools/plotCoverage_rawCounts_woDuplicates.tsv > $out_dir/data/reads_mapped/_deepTools/plotCoverage_output.tsv
+plotCoverage -p $num_threads --ignoreDuplicates --bamfiles $read_files_mapped --outRawCounts $out_dir/data/reads_mapped/_deepTools/plotCoverage_rawCounts_woDuplicates.tsv > $out_dir/data/reads_mapped/_deepTools/plotCoverage_output.tsv
 
-#bamPEFragmentSize -p $num_threads --bamfiles $read_files_mapped --table $out_dir/data/reads_mapped/_deepTools/bamPEFragment_table.tsv --outRawFragmentLengths $out_dir/data/reads_mapped/_deepTools/bamPEFragment_rawLength.tsv
+bamPEFragmentSize -p $num_threads --bamfiles $read_files_mapped --table $out_dir/data/reads_mapped/_deepTools/bamPEFragment_table.tsv --outRawFragmentLengths $out_dir/data/reads_mapped/_deepTools/bamPEFragment_rawLength.tsv
 
-#estimateReadFiltering -p $num_threads --smartLabels --bamfiles $read_files_mapped > $out_dir/data/reads_mapped/_deepTools/estimateReadFiltering_output.tsv
+estimateReadFiltering -p $num_threads --smartLabels --bamfiles $read_files_mapped > $out_dir/data/reads_mapped/_deepTools/estimateReadFiltering_output.tsv
 
 ### need BED files
-#plotEnrichment -p $num_threads --smartLabels --bamfiles $read_files_mapped --outRawCounts $out_dir/data/reads_mapped/_deepTools/plotEnrichment_rawCounts.tsv
+plotEnrichment -p $num_threads --smartLabels --bamfiles $read_files_mapped --outRawCounts $out_dir/data/reads_mapped/_deepTools/plotEnrichment_rawCounts.tsv
 
-#multiqc -f -o $out_dir/data/quality_reports/reads_raw $out_dir/data/reads_raw
-#multiqc -f -o $out_dir/data/quality_reports/reads_prepro $out_dir/data/reads_prepro
+multiqc -f -o $out_dir/data/quality_reports/reads_raw $out_dir/data/reads_raw
+multiqc -f -o $out_dir/data/quality_reports/reads_prepro $out_dir/data/reads_prepro
 multiqc -f -o $out_dir/data/quality_reports/reads_mapped $out_dir/data/reads_mapped
 
 
